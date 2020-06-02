4f29d45b72345e8d8b9bc59227565c8d216c4ec0
Getnet Mekuriyaw <getnetmekuriyaw@gmail.com>
2020-06-02 16:12:57 -0500

    Initial Commit

---
+++ StudyMate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StudyMate
+//
+//  Created by Getnet Mekuriyaw on 6/2/20.
+//  Copyright © 2020 Getnet Mekuriyaw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
